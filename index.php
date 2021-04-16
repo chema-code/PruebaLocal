45b64b2b6d07912ae301337439f09a9d8f1b6228
Chema <chematab@gmail.com>
2021-04-16 18:52:07 +0200

    cambios en main index.php

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <title>Default</title>
 </head>
 <body>
-    <h1>segundo cambio</h1>
     <h1>tercer cambio</h1>
     <h2>cambio en main</h2>
+    <h4>cambio en main</h4>
 </body>
 </html>
